2346b28222a46c6f501af69b7cb90b85d1bf1d91
Daniel R <2902294r@student.gla.ac.uk>
2026-03-29 19:39:33 +0000

    Change anki template
    Move to markdown for anki so no need for latex templating anymore. Was cool but KISS

--- lua/plugins/anki.lua
+++ lua/plugins/anki.lua
@@ -1,18 +1,15 @@
 return {
-	{ 
-		'rareitems/anki.nvim',
-		opts = {
-			tex_support = true,
-			move_cursor_after_creation = true,
-			models = {
-				-- Here you specify which notetype should be associated with which deck
-				-- NoteType = "NOSE",
-				-- ["Basic"] = { "NOSE", "2P" },
-				["Basic2P"] = "2P",
-				["BasicNOSE"] = "NOSE",
-				-- ["NoteType"] = "NoteType",
-				-- ["Super Basic"] = "Deck::ChildDeck",
-			},
+	'rareitems/anki.nvim',
+	opts = {
+		tex_support = true,
+		move_cursor_after_creation = true,
+		models = {
+			-- Here you specify which notetype should be associated with which deck
+			-- NoteType = "NOSE",
+			-- ["Basic"] = { "NOSE", "2P" },
+			["2P"] = "2P",
+			-- ["NoteType"] = "NoteType",
+			-- ["Super Basic"] = "Deck::ChildDeck",
 		},
 	},
 }
